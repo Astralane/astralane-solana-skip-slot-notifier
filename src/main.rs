d414fc38f2682ba0ff91ea5403f230bf4649fde9
sachin <sachin.sanjay@icloud.com>
2024-07-01 15:40:04 +0400

    changed full architecture to grpc

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::env;
-use serenity::builder::ExecuteWebhook;
 use serenity::http::Http;
 use serenity::model::webhook::Webhook;
-use tokio_tungstenite;
-use tokio_tungstenite::tungstenite::protocol::Message;
-use futures_util::{StreamExt,SinkExt};
-use serde_json;
+
 use serde::{Serialize,Deserialize};
 use reqwest;
 use std::{thread, time::Duration};
+use jito_geyser_protos::solana::geyser::{geyser_client::GeyserClient,SubscribeSlotUpdateRequest};
+use tonic::transport::Endpoint;
+use tonic::{Status};
+use std::str::FromStr;
 
 #[derive(Serialize)]
 struct BlockProduction{
@@ -52,24 +52,11 @@ struct LBP{
 struct LBI {
     identity: String
 }
-#[derive(Deserialize,Debug)]
-struct SolanaApiOutput{
-    params: SolanaApiResult,
-}
-
-#[derive(Deserialize,Debug)]
-struct SolanaApiResult{
-    result: SolanaApiSlot,
-}
-#[derive(Deserialize,Debug)]
-struct SolanaApiSlot{
-    slot:u64,
-    parent: u64 //trying wnith parent as there was false positive in slot    
-}
 #[tokio::main]
 async fn main() {
     println!("starting slot bot");
     let token= env::var("BOT_TOKEN").expect("missing env ");
+    let grpc_url=env::var("GRPC_URL").expect("missing env GRPC_URL");
     let api_endpoint= env::var("API").expect("missing api endpoint");
     // You don't need a token when you are only dealing with webhooks.
     let http = Http::new("");
@@ -82,14 +69,12 @@ async fn main() {
         let (first_slot,last_slot)=fetch_boundaries(&api_endpoint).await;
         if prev_first_slot!=first_slot{
             let msg=format!("first and last slot for the epoch is {first_slot} {last_slot}");
-            let builder = ExecuteWebhook::new().content(msg).username("Slot bot");
-            webhook.execute(&http, false, builder).await.expect("Could not execute webhook.");
+            webhook.execute(&http, false, |w| w.content(msg)).await.expect("Could not execute webhook.");
             let leader_slots: Vec<u64> = Vec::new();
             let leader_slots=fetch_leader(&api_endpoint,leader_slots,&first_slot).await;
-            let (completed_slots,skipped_slots,unknown_slots,total_slots) = slot_stream(leader_slots,&api_endpoint,webhook.clone()).await;
+            let (completed_slots,skipped_slots,unknown_slots,total_slots) = slot_stream(leader_slots,&grpc_url,webhook.clone()).await;
             let msg=format!("completed ={completed_slots}, skipped ={skipped_slots}, unknown = {unknown_slots}, total = {total_slots}");
-            let builder = ExecuteWebhook::new().content(msg).username("Slot bot");
-            webhook.execute(&http, false, builder).await.expect("Could not execute webhook."); 
+            webhook.execute(&http, false, |w| w.content(msg)).await.expect("Could not execute webhook."); 
             prev_first_slot=first_slot;             
         }else{
             thread::sleep(Duration::from_millis(5000));
@@ -145,36 +130,28 @@ async fn fetch_leader(api: &String,mut ls: Vec<u64>,base: &u64) -> Vec<u64>{
 async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook) -> (i32,i32,i32,i32) {
 
     let http = Http::new("");
-    let ws_api=format!("wss://{}",api);
-    let (ws,_)= tokio_tungstenite::connect_async(ws_api).await.expect("failed to connect to ws");
-    println!("connected to ws");
-    let (mut w, mut r) =ws.split();
-    let body = BlockProduction{
-        jsonrpc: "2.0".to_string(),
-        id: 777,
-        method: "slotSubscribe".to_string()
-    };
-    let msg = Message::Text(serde_json::to_string(&body).expect("couldn't convert slot_Stream "));
-    w.send(msg).await.expect("failed to send json");
-    let m=r.next().await.unwrap();
-    let m = m.expect("failed to read message");
-    let m = m.into_text().expect("failed to convert to a string");
-    let m = m.as_str();
-    println!("{m}");
+    let grpc_api=format!("http://{}",api);
+    let endpoint = Endpoint::from_str(&grpc_api).unwrap();
+    println!("connected to grpc");
+    let channel = endpoint.connect().await.expect("cannot connect to channel");
+    let mut client = GeyserClient::with_interceptor(channel, intrcptr);
+    let mut stream = client.subscribe_slot_updates(SubscribeSlotUpdateRequest {}).await.expect("couldn't get stream").into_inner();
+
     let mut current_slot: u64 = 0;
     let mut completed_slots = 0;
     let mut skipped_slots = 0;
     let mut unknown_slots =0;
     let total_slots=leader_slots.len();
-    let last_slot=leader_slots[leader_slots.len()-1]+8;//buffer incase you skip the last slot d
+    let last_slot=leader_slots[leader_slots.len()-1]+32;//buffer incase you skip the last slot d
     while current_slot < last_slot{// 0
-        let mm=r.next().await.unwrap();
-        match mm{
+        let slot_grpc=stream.message().await;
+        match slot_grpc{
             Ok(m) => {
-                let m = m.into_text().expect("failed to convert to a string");
-                let m = m.as_str(); 
-                let v:SolanaApiOutput = serde_json::from_str(&m).expect("cannot unpack");
-                current_slot=v.params.result.parent;//chnaged from slots to parent for addded commitment
+                let slot_up=m.unwrap().slot_update.expect("cannot unwrap slot_update");
+                if slot_up.status !=0{
+                    break;
+                }
+                current_slot=slot_up.slot;//chnaged from slots to parent for addded commitment
                 let mut len_vec: usize=leader_slots.len();
                 let mut index =0;
                 while index < len_vec{// TODO restructure so that you first remove unknown then do evenrthing without a loop
@@ -183,7 +160,7 @@ async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook)
                     let ls=leader_slots[index];//92
                     if current_slot > ls  { //don't know what happend could have started the code half way through the epoch
                         let slot_diff=current_slot-ls;//1
-                        if slot_diff > 16{
+                        if slot_diff > 20{
                             unknown_slots+=1;
                             leader_slots.remove(index);                    
                             println!("unknown slots{leader_slots:?}");
@@ -191,8 +168,7 @@ async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook)
                             skipped_slots+=1;
                             let msg=format!("skipped slot {} total skip= {skipped_slots}",leader_slots[0]);
                             leader_slots.remove(index);
-                            let builder = ExecuteWebhook::new().content(msg).username("Slot bot");
-                            webhook.execute(&http, false, builder).await.expect("Could not execute webhook.");  
+                            webhook.execute(&http, false, |w| w.content(msg)).await.expect("Could not execute webhook.");
                             println!("skipped slots {leader_slots:?}");   
                             index+=1;                 
                         }else{
@@ -213,7 +189,10 @@ async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook)
                 }
 
             }
-            Err(_) => {println!("websocket issue"); continue}
+            Err(e) => {println!("grpc issue");
+                println!("{e:?}");
+                continue
+                }
         }
 
         
@@ -221,6 +200,11 @@ async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook)
     }
     (completed_slots,skipped_slots,unknown_slots,total_slots as i32)
 
+}
+fn intrcptr(request: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
+        // request.metadata_mut();
+//            .insert("access-token", self.access_token.parse().unwrap());
+        Ok(request)
 }
 
 
