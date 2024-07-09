041120ff517c83a8c46dc9db0df5de18b7e21316
sachin <sachin.sanjay@icloud.com>
2024-07-09 16:42:18 +0400

    added testnet pipeline

--- src/main.rs
+++ src/main.rs
@@ -167,7 +167,7 @@ async fn slot_stream(mut leader_slots: Vec<u64>, api: &String, webhook: Webhook)
                             println!("unknown slots{leader_slots:?}");
                         }else if slot_diff > 4{
                             skipped_slots+=1;
-                            let msg=format!("skipped slot {} total skip= {skipped_slots} % of skipped = {}",leader_slots[index],skipped_slots as f32/total_slots as f32);
+                            let msg=format!("skipped slot {} total skip= {skipped_slots}, percentage of skipped slots = {}",leader_slots[index],(skipped_slots as f32/total_slots as f32)*100);
                             leader_slots.remove(index);
                             webhook.execute(&http, false, |w| w.content(msg)).await.expect("Could not execute webhook.");
                             println!("skipped slots {leader_slots:?}");   
